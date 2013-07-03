513b6ffd23a08e16098990d9a63381ac2e81b20c
Sergey <tomarech@gmail.com>
2013-07-03 22:39:50 +0400

    PyCharm %TIME%

--- spider_v0.0.py
+++ spider_v0.0.py
@@ -7,25 +7,15 @@ import cookielib
 cookie = cookielib.CookieJar()
 req = urllib2.build_opener(urllib2.HTTPCookieProcessor(cookie))
 req.addheaders = [('User-Agent', 'Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10.6; en-US; rv:1.9.2.11) Gecko/20101012 Firefox/3.6.11'), ]
-resp = req.open('https://login.vk.com/?act=login', urllib.urlencode({'email': '89287664564', 'pass': '*********'}))
+resp = req.open('https://login.vk.com/?act=login', urllib.urlencode({'email': '89281118476', 'pass': '28zrw35q'}))
 
-page = req.open('https://vk.com/issahar').read()
-print page.decode('cp1251')
+page = req.open('https://vk.com/id216378560').read().decode('cp1251')
 
-"""
-page = urllib.urlopen('http://vk.com/issahar/')
-pageWritten = page.read()
-print pageWritten
+xmldata = lxml.html.document_fromstring(page)
+Uname = xmldata.xpath('//div[@class="page_name fl_l ta_l"]/text()')
 
-#pageReady = pageWritten.decode()
+print 'name is %s', Uname[0]
 
-#xmldata = lxml.html.document_fromstring(pageReady)
-
-#temperature = xmldata.xpath('//div[@class="b-thermometer__now"]/text()')
-#clouds = xmldata.xpath('//div[@class="b-info-item b-info-item_type_fact-big"]/text()')
-
-#print 'По данным Yandex сейчас в Москве %s, %s', temperature[0], clouds[0]
-"""
 
 
 
